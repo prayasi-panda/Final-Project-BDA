47e397c45eeb462cf63fd216b3e4e78a18f9b9a8
prayasi-panda <64890160+prayasi-panda@users.noreply.github.com>
2020-12-10 22:26:42 +0530

    Add files via upload

---
+++ BDA/includes/header.php
@@ -0,0 +1,27 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php" style="font-family: 'Caveat', cursive; color: #b22222; font-size: xx-large;">Red Cross</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=aboutus">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=donor">Why Become Donor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Become a Donar</a>
+                    </li>
+
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact us</a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ BDA/index.php
@@ -0,0 +1,60 @@
+<?php include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Red Cross</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+
+ <!--Banner Image-->
+   <div id="banner_image1">
+   </div>      
+<!--End of Banner Image-->
+      
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
